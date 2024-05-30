5dcdd63eee3bd28301c82f555eb90bfd024f398e
lolmageap <ekxk1234@naver.com>
2024-05-30 23:05:37 +0900

    project init

---
+++ src/main/kotlin/com/example/excelsupport/ExcelSupportApplication.kt
@@ -0,0 +1,11 @@
+package com.example.excelsupport
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcelSupportApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExcelSupportApplication>(*args)
+}
