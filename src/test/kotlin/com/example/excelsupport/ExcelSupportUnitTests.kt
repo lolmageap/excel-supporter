d1c1ca3e928c7b09ef73eefa65c3c17a7a59f49b
lolmageap <ekxk1234@naver.com>
2024-06-02 21:49:58 +0900

    separate excel support unit test

--- src/test/kotlin/com/example/excelsupport/ExcelSupportUnitTests.kt
+++ src/test/kotlin/com/example/excelsupport/ExcelSupportUnitTests.kt
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import java.io.File
 
 @SpringBootTest
-class ExcelSupportTests(
+class ExcelSupportUnitTests(
     @Autowired private val excelManager: ExcelManager,
     @Autowired private val httpServletResponse: HttpServletResponse,
 ) {
@@ -34,7 +33,6 @@ class ExcelSupportTests(
         val person2 = Person("Jane", 25, 170)
         val person3 = Person("Tom", 27, 175)
         val people = listOf(person, person2, person3)
-
         val file = excelManager.createFile(Person::class)
         excelManager.writeBody(file, people)
         file.delete()
@@ -47,7 +45,6 @@ class ExcelSupportTests(
         val person2 = Person("Jane", 25, 170)
         val person3 = Person("Tom", 27, 175)
         val people = listOf(person, person2, person3)
-
         val txtFile = File("test.txt")
         assertThrows<IllegalArgumentException> { excelManager.writeBody(txtFile, people) }
         txtFile.delete()
@@ -56,31 +53,7 @@ class ExcelSupportTests(
     @Test
     @DisplayName("파일 다운로드")
     fun downloadFile() {
-        val person = Person("John", 30, 180)
-        val person2 = Person("Jane", 25, 170)
-        val person3 = Person("Tom", 27, 175)
-        val people = listOf(person, person2, person3)
-
         val file = excelManager.createFile(Person::class)
-        excelManager.downloadFile(file, httpServletResponse)
-    }
-
-    @Test
-    @DisplayName("엑셀 파일을 생성 후 대용량의 데이터를 추가하고 다운로드")
-    fun createAndDownloadFile() {
-        val people = (1..1_000_000).map { Person("John", 30, 180) }
-
-        val pageable = Pageable(1, 100_000, "name", "asc")
-        val chunked = pageable.size.chunked(2000)
-        val file = excelManager.createFile(Person::class)
-
-        repeat(chunked.size) {
-            val start = it * 2000
-            val end = if (it == chunked.size - 1) pageable.size else (it + 1) * 2000
-            val subList = people.subList(start, end)
-            excelManager.writeBody(file, subList)
-        }
-
         excelManager.downloadFile(file, httpServletResponse)
     }
 }
