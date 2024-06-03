17f8b1776a8bc6b2d69b2265ab3f63ba16473f2a
lolmageap <ekxk1234@naver.com>
2024-06-03 22:21:39 +0900

    delete custom pageable class

--- src/test/kotlin/com/example/excelsupport/IntExtensionKtTest.kt
+++ src/test/kotlin/com/example/excelsupport/IntExtensionKtTest.kt
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.Test
 
 class IntExtensionKtTest {
     @Test
-    @DisplayName("Pageable 객체의 크기 100,000을 주어진 크기 2,000으로 나누어 50개의 청크 리스트로 반환하는 기능")
+    @DisplayName("100,000을 2,000으로 나누어 50개의 청크 리스트로 반환하는 기능")
     fun chunked() {
-        val pageable = Pageable(1, 100_000, "name", "asc")
-        val chunked = pageable.size.chunked(2_000)
+        val chunked = 100_000.chunked(2_000)
 
         assert(chunked.size == 50)
         assert(chunked[0] == 2_000)
